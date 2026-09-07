4e4d610d7ce20d62a105c17d575e3a11d51a09aa
KP <175162338+krishpdev@users.noreply.github.com.>
2026-09-06 22:08:23 -0400

    create server.cpp

---
+++ server.cpp
@@ -0,0 +1,62 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/ip.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static void do_something(int connfd) {
+  char rbuf[64] = {};
+  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+  if (n < 0) {
+    msg("read() error");
+    return;
+  }
+  fprintf(stderr, "client says: %s\n", rbuf);
+
+  char wbuf[] = "world";
+  write(connfd, wbuf, strlen(wbuf));
+}
+int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+if (fd < 0) {
+  throw std::system_error(errno, std::system_category(), "socket() failed");
+}
+
+int value = 1;
+setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value));
+
+struct sockaddr_in addr = {};
+addr.sin_family = AF_INET;
+addr.sin_port = htons(1212);
+addr.sin_addr.s_addr = INADDR_ANY;
+
+int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+
+if (rv) {
+  throw std::system_error(errno, std::system_category(), "bind() failed");
+}
+
+rv = listen(fd, SOMAXCONN);
+
+if (rv) {
+  throw std::system_error(errno, std::system_category(), "listen() failed");
+}
+
+while (true) {
+  struct soccaddr_in client_addr = {};
+  socklen_t addrlen = sizeof(client_addr);
+  int connfd = accept(fd, (struct soccaddr *)&client_addr, &addrlen);
+
+  if (connfd < 0) {
+    continue;
+  }
+
+  do_something(connfd);
+  close(connfd);
+}
