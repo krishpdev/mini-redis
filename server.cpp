41660bd6face0599c882f71bad93f88a3738461a
KP <175162338+krishpdev@users.noreply.github.com.>
2026-09-06 23:24:24 -0400

    clean up code

--- server.cpp
+++ server.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <system_error>
 #include <unistd.h>
 
 static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
@@ -22,41 +23,45 @@ static void do_something(int connfd) {
   char wbuf[] = "world";
   write(connfd, wbuf, strlen(wbuf));
 }
-int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-if (fd < 0) {
-  throw std::system_error(errno, std::system_category(), "socket() failed");
-}
+int main() {
 
-int value = 1;
-setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value));
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-struct sockaddr_in addr = {};
-addr.sin_family = AF_INET;
-addr.sin_port = htons(1212);
-addr.sin_addr.s_addr = INADDR_ANY;
-
-int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-
-if (rv) {
-  throw std::system_error(errno, std::system_category(), "bind() failed");
-}
-
-rv = listen(fd, SOMAXCONN);
-
-if (rv) {
-  throw std::system_error(errno, std::system_category(), "listen() failed");
-}
-
-while (true) {
-  struct soccaddr_in client_addr = {};
-  socklen_t addrlen = sizeof(client_addr);
-  int connfd = accept(fd, (struct soccaddr *)&client_addr, &addrlen);
-
-  if (connfd < 0) {
-    continue;
+  if (fd < 0) {
+    throw std::system_error(errno, std::system_category(), "socket() failed");
   }
 
-  do_something(connfd);
-  close(connfd);
+  int value = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value));
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(1212);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+
+  if (rv) {
+    throw std::system_error(errno, std::system_category(), "bind() failed");
+  }
+
+  rv = listen(fd, SOMAXCONN);
+
+  if (rv) {
+    throw std::system_error(errno, std::system_category(), "listen() failed");
+  }
+
+  while (true) {
+    struct sockaddr_in client_addr = {};
+    socklen_t addrlen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+
+    if (connfd < 0) {
+      continue;
+    }
+
+    do_something(connfd);
+    close(connfd);
+  }
 }
