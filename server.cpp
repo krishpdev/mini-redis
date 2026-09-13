2c43a66e813fe2b562859a0acc83c5556d1f7990
KP <175162338+krishpdev@users.noreply.github.com.>
2026-09-12 20:01:32 -0400

    handle buffer logic

--- server.cpp
+++ server.cpp
@@ -2,12 +2,14 @@
 #include <assert.h>
 #include <cstdint>
 #include <errno.h>
+#include <fcntl.h>
 #include <netinet/ip.h>
 #include <poll.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <system_error>
@@ -45,6 +47,84 @@ struct Connection {
   std::vector<uint8_t> read_buffer;
   std::vector<uint8_t> write_buffer;
 };
+
+static void fd_set_nb(int fd) {
+  fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+}
+
+static void buffer_insert(std::vector<uint8_t> &buffer, const uint8_t *data,
+                          size_t len) {
+  buffer.insert(buffer.end(), data, data + len);
+}
+
+static void buffer_erase(std::vector<uint8_t> &buffer, size_t offset,
+                         size_t len) {
+  buffer.erase(buffer.begin() + offset, buffer.begin() + offset + len);
+}
+
+static bool try_one_request(Connection *connection) {
+
+  if (connection->read_buffer.size() < 4) {
+    return false;
+  }
+
+  uint32_t len = 0;
+
+  mempcpy(&len, connection->read_buffer.data(), 4);
+
+  if (len > k_max_msg) {
+    connection->want_close = true;
+    return false;
+  }
+
+  if (4 + len > connection->read_buffer.size()) {
+    return false;
+  }
+
+  const uint8_t *request = connection->read_buffer.data() + 4;
+
+  buffer_insert(connection->write_buffer, (const uint8_t *)&len, 4);
+  buffer_insert(connection->write_buffer, request, len);
+
+  buffer_erase(connection->read_buffer, 0, 4 + len);
+
+  return true;
+}
+
+static Connection *handle_accept(int fd) {
+  struct sockaddr_in client_addr = {};
+
+  socklen_t addrlen = sizeof(client_addr);
+
+  int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+
+  if (connfd < 0) {
+    return nullptr;
+  }
+
+  fd_set_nb(connfd);
+
+  Connection *connection = new Connection();
+  connection->fd = connfd;
+  connection->want_read = true;
+  return connection;
+}
+
+static void handle_read(Connection *connection) {
+  char buf[1024] = {};
+  ssize_t n = read(connection->fd, buf, sizeof(buf));
+
+  if (n <= 0) {
+    connection->want_close = true;
+    return;
+  }
+
+  fprintf(stderr, "client says: %.*s\n", (int)n, buf);
+
+  buffer_insert(connection->read_buffer, buf, (size_t)n);
+
+  try_one_request(connection);
+}
 
 int main() {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -103,7 +183,7 @@ int main() {
       pollfds.push_back(pfd);
     }
 
-    int rv = poll(pollfds.data(), pollfds.size(), -1);
+    int rv = poll(pollfds.data(), (nfds_t)pollfds.size(), -1);
 
     if (rv < 0) {
       if (errno == EINTR) {
@@ -119,6 +199,26 @@ int main() {
           fd_connections.resize(connection->fd + 1);
         }
         fd_connections[connection->fd] = connection;
+      }
+    }
+
+    for (size_t i = 1; i < pollfds.size(); i++) {
+      uint32_t ready = pollfds[i].revents;
+
+      Connection *connection = fd_connections[pollfds[i].fd];
+
+      if (ready & POLLIN) {
+        handle_read(connection);
+      }
+
+      if (ready & POLLOUT) {
+        handle_write(connection);
+      }
+
+      if (ready & POLLERR || connection->want_close) {
+        (void)close(connection->fd);
+        fd_connections[connection->fd] = nullptr;
+        delete connection;
       }
     }
   }
